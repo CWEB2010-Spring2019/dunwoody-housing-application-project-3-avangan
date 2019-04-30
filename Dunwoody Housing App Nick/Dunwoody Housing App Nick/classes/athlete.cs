dc1f910c94f14b11581bc2196ea9fb769a65c409
avangan <38505183+avangan@users.noreply.github.com>
2019-04-29 23:50:39 -0500

    Lots of Changes

---
+++ Dunwoody Housing App Nick/Dunwoody Housing App Nick/classes/athlete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dunwoody_Housing_App_Nick.classes
+{
+    class athlete : res
+    {
+        public athlete(string first, string last, int id, int room, int floor, double rent)
+        {
+            {
+                this.first = first;
+                this.last = last;
+                this.id = id;
+                this.room = room;
+                this.floor = floor;
+                this.rent = rent;
+
+            }
+        }
+    }
+}
+
+    
